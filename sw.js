efc3f4b2a45a26d0b9615cd12992d96b4a3fc8f9
jack850628 <jack850628@gmail.com>
2021-02-26 23:11:29 +0800

    新增PWA功能

---
+++ sw.js
@@ -0,0 +1,37 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('v1').then(function(cache) {
+        return cache.addAll([
+        //   '/sw-test/',
+        //   '/sw-test/index.html',
+        //   '/sw-test/style.css',
+        //   '/sw-test/app.js',
+        //   '/sw-test/image-list.js',
+        //   '/sw-test/star-wars-logo.jpg',
+        //   '/sw-test/gallery/bountyHunters.jpg',
+        //   '/sw-test/gallery/myLittleVader.jpg',
+        //   '/sw-test/gallery/snowTroopers.jpg'
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(caches.match(event.request).then(function(response) {
+      if (response !== undefined) {
+        return response;
+      } else {
+        return fetch(event.request).then(function (response) {
+          let responseClone = response.clone();
+          
+          caches.open('v1').then(function (cache) {
+            cache.put(event.request, responseClone);
+          });
+          return response;
+        })
+        // .catch(function () {
+        //   return caches.match('/sw-test/gallery/myLittleVader.jpg');
+        // });
+      }
+    }));
+  });
