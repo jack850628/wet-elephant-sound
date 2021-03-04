b23613c71114f731ce9b8dcfe2fe9cd309e44ceb
jack850628 <jack850628@gmail.com>
2021-03-05 00:13:28 +0800

    觸發快取更新 v1.5.0305

--- sw.js
+++ sw.js
@@ -1,16 +1,10 @@
 self.addEventListener('install', function(event) {
     event.waitUntil(
-      caches.open('v1').then(function(cache) {
+      caches.open('v1.5.0305').then(function(cache) {
         return cache.addAll([
-        //   '/sw-test/',
-        //   '/sw-test/index.html',
-        //   '/sw-test/style.css',
-        //   '/sw-test/app.js',
-        //   '/sw-test/image-list.js',
-        //   '/sw-test/star-wars-logo.jpg',
-        //   '/sw-test/gallery/bountyHunters.jpg',
-        //   '/sw-test/gallery/myLittleVader.jpg',
-        //   '/sw-test/gallery/snowTroopers.jpg'
+          './',
+          './index.html',
+          './audios.js',
         ]);
       })
     );
