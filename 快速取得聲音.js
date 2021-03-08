06b15089c62b2b05e9e4791a36d480b9fa3b4e0b
jack850628 <jack850628@gmail.com>
2021-03-09 01:52:42 +0800

    讓腳本更好用

--- 快速取得聲音.js
+++ 快速取得聲音.js
@@ -1,6 +1,7 @@
-id = 504;
-data = [];
-document.querySelectorAll('#content > div:nth-child(2) > div:nth-child(507) ~ .instant').forEach((i)=>{
+var id = 504;
+var data = [];
+var div = '#content > div:nth-child(2) > div:nth-child(507)';
+document.querySelectorAll(div + ' ~ .instant').forEach((i)=>{
     data.push({
         name: i.querySelector('.instant-link').innerText,
         url: i.querySelector('.small-button').getAttribute('onmousedown').match(/.+?\('(.*?)'\)/)[1],
