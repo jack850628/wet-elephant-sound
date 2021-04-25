494c3e4e1aa43a3299eae01e93350ed61f1eab37
jack850628 <jack850628@gmail.com>
2021-04-25 21:58:49 +0800

    更新語音

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 var APP_PREFIX = 'wet-elephant-sound_';
-var VERSION = 'v1.7.0423.2';
+var VERSION = 'v1.7.0425';
 var CACHE_NAME = APP_PREFIX + VERSION
 var URLS = [                            
   './',
